b09aad74a85cfd06352c6498b58b768d351ccd93
richard <richard.orofeo@synacy.com>
2015-08-12 10:19:12 +0800

    fixed dataservice.js and can calculate bmi and bmr

--- healthapp/bmr/bmr.js
+++ healthapp/bmr/bmr.js
@@ -7,20 +7,12 @@
 
 		Bmr.$inject = ['dataservice', '$scope'];
 
-<<<<<<< HEAD
-			/*function calcBMR() {
-				return dataservice.calcBMR().then(function(data) {
-					vm.bmr = data;
-					return vm.bmr;
-				});
-			}*/
-=======
 		function Bmr(dataservice,$scope) {
 
 			$scope.getBmr = function() {
 				$scope.answer = dataservice.calcBMR($scope.gender, $scope.weight, $scope.height, $scope.age);
 			}
->>>>>>> 3faa1e59f67aae0a7bfe274393cb0d85b76ce21a
+
 		}
 
 })();
--- healthapp/core/dataservice.js
+++ healthapp/core/dataservice.js
@@ -14,42 +14,22 @@
 
 			
             function calcBMI(weight, height) {
-				var bmi = (weight  / (height * height)) * 703;
-				return(bmi);
+                var bmi = (weight / (height * height)) * 703;
+                return bmi;
 
-<<<<<<< HEAD
-            function calcBMR(){
-                $scope.BMR = null;
-                var BMR = $scope.BMR;
-
-                if ($scope.gender == 'male') {
-                    BMR = 66 + (6.23 * $scope.weight) + (12.7 * $scope.height) - (6.8 * $scope.age);
-                } else {
-                    BMR = 655 + (4.35 * $scope.weight) + (4.7 * $scope.height) - (4.7 * $scope.age);
-                }
-                $scope.BMR = BMR;
-                return(BMR);
             }
 
-			function calcDKR(){
-				$scope.DKR = $scope.BMR * $scope.exercise;
-				var DKR = $scope.DKR;
-				return(DKR);
-			}
+            function calcBMR(gender, weight, height, age){
+                var bmr = this;
 
-=======
-			}
+                if (gender == 'male') {
+                    bmr = 66 + (6.23 * weight) + (12.7 * height) - (6.8 * age);
+                } else {
+                    bmr = 655 + (4.35 * weight) + (4.7 * height) - (4.7 * age);
+                }
+                return(bmr);
+            }
 
-			function calcBMR(gender, weight, height, age){
-				var bmr = this;
-
-				if (gender == 'male') {
-				bmr = 66 + (6.23 * weight) + (12.7 * height) - (6.8 * age);
-				} else {
-				bmr = 655 + (4.35 * weight) + (4.7 * height) - (4.7 * age);
-				}
-				return(bmr);
-				}
 
 			function calcDKR(exercise,$scope){
 				var dkr = this;
@@ -59,7 +39,6 @@
 				return(dkr);
 			}
 
->>>>>>> 3faa1e59f67aae0a7bfe274393cb0d85b76ce21a
             return service;
 
 		});
