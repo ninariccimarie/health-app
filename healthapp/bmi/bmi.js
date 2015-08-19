e5ff91911002b2f93dae078f722282fe8866cce4
richard <richard.orofeo@synacy.com>
2015-08-19 13:14:44 +0800

    refactored to a single button that computes all statistics.

--- healthapp/bmi/bmi.js
+++ healthapp/bmi/bmi.js
@@ -1,29 +1,69 @@
 (function(){
-	'use strict';
+    'use strict';
 
-	angular
-		.module('app.core')
-		.controller('Bmi', Bmi);
+    angular
+        .module('app.core')
+        .controller('Bmi', Bmi);
 
-		Bmi.$inject = ['dataservice', '$scope'];
+    Bmi.$inject = ['dataservice', '$scope'];
 
-		function Bmi(dataservice,$scope) {
-			
-            $scope.getBmi = function(){
-                 $scope.result = '';
+    function Bmi(dataservice,$scope) {
 
-                 $scope.answer = dataservice.calcBMI($scope.weight, $scope.height);
+        $scope.compute = function () {
+            activate();
+        };
 
-                    if($scope.answer < 18.5){
-                        $scope.result = "Underweight";
-                    }else if ($scope.answer >= 18.5 && $scope.answer < 25) {
-                        $scope.result = "Normal";
-                    } else if ($scope.answer >= 25 && $scope.answer < 30) {
-                        $scope.result = "Overweight";
-                    } else {
-                        $scope.result = "Obese";
-                    }
-                return $scope.result;
+        function getBmi() {
+            $scope.result = '';
+            console.log('clicked');
+            $scope.getBmi = dataservice.calcBMI($scope.weight, $scope.height)
+
+            if ($scope.answer < 18.5) {
+                $scope.result = "Underweight";
+            } else if ($scope.answer >= 18.5 && $scope.answer < 25) {
+                $scope.result = "Normal";
+            } else if ($scope.answer >= 25 && $scope.answer < 30) {
+                $scope.result = "Overweight";
+            } else {
+                $scope.result = "Obese";
+            }
+
+            return $scope.result;
+        }
+
+        function getBmr(){
+            console.log('compute bmr');
+            $scope.getBmr = dataservice.calcBMR($scope.gender, $scope.weight, $scope.height, $scope.age);
+        }
+
+        function getBpRate(){
+            console.log('Get BP Rate');
+
+            $scope.getBpRate = function(){
+                if ($scope.systolic <= 120 && $scope.diastolic <= 80) {
+                    return ("Normal");
+                } else if ($scope.systolic >= 120 && $scope.systolic < 140 && $scope.diastolic >= 80 && $scope.diastolic < 90) {
+                    return ("Prehypertension");
+                } else if ($scope.systolic >= 140 && $scope.systolic < 160 && $scope.diastolic >= 90 && $scope.diastolic < 100) {
+                    return ("High Blood Pressure (Hypertension) Stage 1");
+                } else if ($scope.systolic >= 160 && $scope.systolic <= 180 && $scope.diastolic >= 100 && $scope.diastolic <= 110) {
+                    return ("High Blood Pressure (Hypertension) Stage 2");
+                } else if ($scope.systolic > 180 && $scope.diastolic > 110) {
+                    return ("Hypertensive Crisis (Emergency care needed!)");
+                }
             };
-		}
+        }
+
+        function getDkr(){
+            console.log('compute dkr');
+            $scope.answer = dataservice.calcDKR(dataservice.calcBMR($scope.gender, $scope.weight, $scope.height, $scope.age), $scope.exercise);
+        }
+
+        function activate(){
+            getBmi();
+            getBmr();
+            getBpRate();
+            getDkr();
+        }
+    }
 })();
